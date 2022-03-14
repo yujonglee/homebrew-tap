97cf5db533eae9e9ccdf893e4297f076979285f7
yujong-lee <yujonglee@g.skku.edu>
2022-03-14 13:43:53 +0900

    letsdmmf: v0.2.0

--- Formula/letsdmmf.rb
+++ Formula/letsdmmf.rb
@@ -1,9 +1,9 @@
 class Letsdmmf < Formula
     desc "CLI for traversing DMMF of your Prisma schema"
     homepage "https://github.com/yujong-lee/letsdmmf"
-    url "https://github.com/yujong-lee/letsdmmf/releases/download/0.1.0/letsdmmf.tar.gz"
-    sha256 "10239a45d3f4e085d9b6f87b7286043330cec6ac3c558a11031fcd57d35174e0"
-    version "0.1.0"
+    url "https://github.com/yujong-lee/letsdmmf/releases/download/0.2.0/letsdmmf.tar.gz"
+    sha256 "bb12fd450bccd44314599100f0d79dfd356a0178e71eb2ded857fe0a867337aa"
+    version "0.2.0"
   
     def install
       bin.install "letsdmmf"
