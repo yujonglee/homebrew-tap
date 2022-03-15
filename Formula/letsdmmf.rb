fc7166b5dced044d4969d41e34ef1fd560ee591e
yujong-lee <yujonglee@g.skku.edu>
2022-03-15 21:56:22 +0900

    letsdmmf: v0.2.3 - fix

--- Formula/letsdmmf.rb
+++ Formula/letsdmmf.rb
@@ -3,7 +3,7 @@ class Letsdmmf < Formula
     homepage "https://github.com/yujong-lee/letsdmmf"
     url "https://github.com/yujong-lee/letsdmmf/releases/download/0.2.3/letsdmmf.tar.gz"
     sha256 "8e520cbd821ed3cbaf1352d403dee3a4537e7050810cc43dcbe9a0a3c1c9ba7e"
-    version "0.2.2"
+    version "0.2.3"
     depends_on  "jless"
   
     def install
