dd9866a46033649e8e1f26c9446975d8b2e341b5
Jayden <yujonglee@g.skku.edu>
2022-08-04 21:00:21 +0900

    Update letsdmmf.rb

--- Formula/letsdmmf.rb
+++ Formula/letsdmmf.rb
@@ -2,7 +2,7 @@ class Letsdmmf < Formula
     desc "CLI for traversing DMMF of Prisma schema"
     homepage "https://github.com/yujong-lee/letsdmmf"
     url "https://github.com/yujong-lee/letsdmmf/releases/download/0.3.0/letsdmmf.tar.gz"
-    sha256 "c8215e94b4decb724bd4ff27bd9384d9209c4d062dc95b2e31db3ccd8633b3b3"
+    sha256 "2127e0620e1475f957d2429b05395c08e818928a19ba42d3d9651b87f3169be9"
     version "0.3.0"
     depends_on  "jless"
   
